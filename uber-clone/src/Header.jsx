d8c866b9c182b9925be6b30d3085fe0dd333d5e3
Rajiv <lucky81205@gmail.com>
2025-01-24 23:03:10 +0530

    header

---
+++ uber-clone/src/Header.jsx
@@ -0,0 +1,30 @@
+function Header(){
+    return(
+        <header className="header">
+      <nav className="header__nav">
+        {/* Left Section */}
+        <div className="header__left">
+          <span className="header__logo">Uber</span>
+          <ul className="header__menu">
+            <li>Ride</li>
+            <li>Drive</li>
+            <li>Business</li>
+            <li>About</li>
+          </ul>
+        </div>
+        {/* Right Section */}
+        <div className="header__right">
+          <ul className="header__options">
+            <li>EN</li>
+            <li>Help</li>
+            <li>Log in</li>
+          </ul>
+          <button className="header__signup">Sign up</button>
+        </div>
+      </nav>
+    </header>
+
+    )
+}
+
+export default Header
