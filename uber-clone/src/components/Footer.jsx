7d459e0d2284e2b4767d61871c153665fcdc0d79
Rajiv <lucky81205@gmail.com>
2025-01-25 09:00:19 +0530

    footer

---
+++ uber-clone/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+function Footer(){
+    const year = new Date().getFullYear()
+
+    return(
+        <footer>
+            <div className="footer-sec-1"></div>
+        </footer>
+
+        
+    )
+}
+
+export default Footer
