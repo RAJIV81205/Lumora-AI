fe8c33e0bdc0678ed6a8f9cba46688a1a85350b6
Rajiv <lucky81205@gmail.com>
2025-04-11 22:23:55 +0530

    Update index.html title and favicon; add ScrollToTop component in App.jsx to reset scroll position on route changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,28 @@
-import React from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router'
+import React, { useEffect } from 'react'
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router'
 import LandingPage from './components/pages/LandingPage'
 import LoginForm from './components/pages/LoginForm'
 import SignupForm from './components/pages/SignupForm'
 
+// ScrollToTop component to reset scroll position on route changes
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
+
 const App = () => {
   return (
     <Router>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" index element={<LoginForm />} />
         <Route path="/signup" element={<SignupForm />} />
-
-
       </Routes>
     </Router>
   )
