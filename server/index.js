75d595cc8111dd66f831749362fdb026d813b072
Rajiv <lucky81205@gmail.com>
2025-04-11 17:16:43 +0530

    Add UserRoutes to server index.js for API endpoint management

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv"
 import connectDB from "./config/db.js";
+import UserRoutes from "./routes/UserRoutes.js";
 
 
 const app =express();
@@ -18,6 +19,11 @@ app.use(cors(
 
 const PORT = process.env.PORT
 connectDB();
+
+app.use("/api", UserRoutes);
+
+
+
 
 
 
