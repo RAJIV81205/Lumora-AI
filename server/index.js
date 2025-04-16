5d9fa9a8e1b3b6af6faa111b5e9b732cbe6116dc
Rajiv <lucky81205@gmail.com>
2025-04-16 16:55:47 +0530

    Refactor user routes by renaming userRoutes to router and updating import paths. Remove obsolete UserRoutes.js file to streamline routing structure.

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv"
 import connectDB from "./config/db.js";
-import userRoutes from "./routes/userRoutes.js";
+import router from "./routes/user-routes.js";
 
 
 const app = express();
@@ -18,7 +18,7 @@ app.use(cors({
 const PORT = process.env.PORT || 5000;
 connectDB();
 
-app.use("/api", userRoutes);
+app.use("/api", router);
 
 
 app.listen(PORT, () => {
